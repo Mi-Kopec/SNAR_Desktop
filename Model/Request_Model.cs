3261d0c70d3b9cc8b4ca55a8a3c100a7c6cac7ab
Michał Kopeć <michal.kopec96@gmail.com>
2019-12-28 13:00:13 +0100

    Adding MVVM template to RequestTab

---
+++ Model/Request_Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNAR_sheet.Request_Model
+{
+    class Request_Model
+    {
+    }
+}
---
+++ ViewModel/Request_ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SNAR_sheet.Request_Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace SNAR_sheet.Request_ViewModel
+{
+    class Request_ViewModel
+    {
+    }
+}
